caa1e48873f6b65e3e325df50e732689d2909858
TymekBak <tymoteusz.fr.bak@gmail.com>
2026-03-24 08:46:10 -0700

    Update MahajanRecorder.cpp

--- Folder 5 - Bifurcation Diagram Generation (Stage 2)/MahajanRecorder.cpp
+++ Folder 5 - Bifurcation Diagram Generation (Stage 2)/MahajanRecorder.cpp
@@ -250,7 +250,7 @@ int main(int argc, char *argv[]) {
             Vm_old = VM.v;
             Cai_old = VM.ci;
 
-            //At the transition point to the new BCL, record the outputs for the present BCL.
+            //At the transition point to the new BCL, record the outputs for the present BCL. The first 21 beats are skipped to allow the code to learn how to compute these biomarkers prior to the beginning of the recording interval.
             if(abs(t - BCL*n) <= (dt/10)){
                 if(n > 20){
                     Validation_Parameters_n << BCL << "\t";
