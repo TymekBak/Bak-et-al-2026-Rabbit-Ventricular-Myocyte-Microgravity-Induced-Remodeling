d8138287dc8b7b1abd5c3e9fb53c80b1f46b4898
TymekBak <tymoteusz.fr.bak@gmail.com>
2026-03-26 12:17:57 -0700

    Add files via upload

--- Folder 07 - Alternans Threshold Search Analysis (Action Potential)/CalciumTransientAnalysisFull.py
+++ Folder 07 - Alternans Threshold Search Analysis (Action Potential)/CalciumTransientAnalysisFull.py
@@ -8,7 +8,7 @@ import pandas
 outputfile = np.zeros((1001,17))
 
 j = 0
-while(j <= 5):
+while(j <= 1000):
     #Load the necessary data.
     path = "Outcomes_t/Baseline_P" + str(j) + ".txt"
     a = pathlib.Path(path)
@@ -120,12 +120,12 @@ while(j <= 5):
 
     #Store the necessary values in an array.
     outputfile[j][0] = Threshold
-    outputfile[j][1] = APD1_Post
-    outputfile[j][2] = APD2_Post
+    outputfile[j][1] = abs(APD1_Post)
+    outputfile[j][2] = abs(APD2_Post)
     outputfile[j][3] = Cai_Max1_Post - Cai_ED1_Post
     outputfile[j][4] = Cai_Max2_Post - Cai_ED2_Post
-    outputfile[j][5] = APD1_Pre
-    outputfile[j][6] = APD2_Pre
+    outputfile[j][5] = abs(APD1_Pre)
+    outputfile[j][6] = abs(APD2_Pre)
     outputfile[j][7] = Cai_Max1_Pre - Cai_ED1_Pre
     outputfile[j][8] = Cai_Max2_Pre - Cai_ED2_Pre
 
