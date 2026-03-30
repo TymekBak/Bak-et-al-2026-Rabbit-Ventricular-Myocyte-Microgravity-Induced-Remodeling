3ead5a933aea537700278d0d1452d2a94ef35600
TymekBak <tymoteusz.fr.bak@gmail.com>
2026-03-30 13:10:37 -0700

    Update CalciumTransientAnalysisFollowUp.py

--- Folder 07 - Alternans Threshold Search Analysis (Action Potential)/CalciumTransientAnalysisFollowUp.py
+++ Folder 07 - Alternans Threshold Search Analysis (Action Potential)/CalciumTransientAnalysisFollowUp.py
@@ -7,7 +7,7 @@ import pandas
 j = 0
 
 while(j < 23):
-    #Cycle through each individual that requires a more through analysis
+    #Cycle through each individual that requires a more detailed analysis
     ID = [23,119,147,313,458,519,599,472]
     #Load the necessary data
     path = "Outcomes_t/Baseline_P" + str(ID[j]) + ".txt"
