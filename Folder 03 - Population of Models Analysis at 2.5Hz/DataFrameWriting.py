8d485d79f549735083fc92f82c02d75c2e300db5
TymekBak <tymoteusz.fr.bak@gmail.com>
2026-03-30 10:45:24 -0700

    Update DataFrameWriting.py

--- Folder 03 - Population of Models Analysis at 2.5Hz/DataFrameWriting.py
+++ Folder 03 - Population of Models Analysis at 2.5Hz/DataFrameWriting.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import scipy.integrate
 import pandas
 
+#Define a function that extracts the input and output data for a specific individual (beat A).
 def ExtractIndividual(i,version):
     path = "Individuals/" + version + "_P" + str(i) + ".txt"
     a = pathlib.Path(path)
@@ -16,6 +17,7 @@ def ExtractIndividual(i,version):
     C = np.append(C0,Cn[1:len(Cn)])
     return(C)
 
+#Use the ExtractIndividual() to create a DataFrame and a .csv summary file for the simulation results.
 p = np.array(["g_{RyR}","u","v_{up}","[Ca^{2+}]_{up}","g_{LTCC}","v_{th}","g_{to,s}","g_{to,f}","g_{NaCa}","g_{Ks}","g_{Kr}","g_{Na}","g_{K1}","g_{NaK}","g_{leak}","APD", "Cai_{max}", "Cai_{min}", "CaSR_{max}", "CaSR_{min}", "Nai_{max}", "Nai_{min}", "CaTD_{50}", "TTP"])
 values = np.zeros((1001,len(p)))
 print(len(p))
@@ -36,6 +38,7 @@ DataFrame = pandas.DataFrame(DataFrame)
 print(DataFrame)
 DataFrame.to_csv("SimulationData_BCL400A.csv")
 
+#Define a function that extracts the input and output data for a specific individual (beat B). This is repeated so that any alternans that are present do not influence the results.
 def ExtractIndividual(i,version):
     path = "Individuals/" + version + "_P" + str(i) + ".txt"
     a = pathlib.Path(path)
@@ -48,6 +51,7 @@ def ExtractIndividual(i,version):
     C = np.append(C0,Cn[1:len(Cn)])
     return(C)
 
+#Use the ExtractIndividual() to create a DataFrame and a .csv summary file for the simulation results.
 values = np.zeros((1001,len(p)))
 print(len(p))
 i = 0
